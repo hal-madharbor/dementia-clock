f596f1e0730e39bfa48f80f9b81ca9c2115242d6
hal-madharbor <hal@madharbor.com>
2025-11-08 16:25:26 -0500

    Handle missing photo ID in getPhoto function
    Add error handling for missing photo ID in getPhoto function

--- js/photo-db.js
+++ js/photo-db.js
@@ -55,6 +55,10 @@ function savePhoto(id, base64, caption) {
  * @returns {Promise<Object>} - Photo data
  */
 function getPhoto(id) {
+    if (!id) {
+        console.error('getPhoto called without an id');
+        return Promise.reject(new Error('No photo ID provided'));
+    }
     return new Promise((resolve, reject) => {
         const transaction = db.transaction([STORE_NAME], 'readonly');
         const store = transaction.objectStore(STORE_NAME);
